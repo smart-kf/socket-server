b5c2612f7d0a79bcd6616755e1e27735e9a49e3d
zhangmj <zhangmengjin@qiyou.cn>
2025-01-09 22:41:00 +0800

    feat: 新增上线/离线事件

--- domain/service/websocket/conn.go
+++ domain/service/websocket/conn.go
@@ -3,6 +3,7 @@ package websocket
 import (
 	"context"
 
+	"goim3/domain/common/constant"
 	"goim3/domain/websocket"
 	"goim3/domain/websocket/model"
 )
@@ -12,10 +13,31 @@ func CreateConn(ctx context.Context, conn *model.Conn) error {
 	if err := connAgg.Create(ctx); err != nil {
 		return err
 	}
-	return nil
+	// 发布一个上线事件.
+	agg := websocket.FactoryMessageAgg(
+		ctx, &model.Message{
+			Event:     constant.Online,
+			Platform:  conn.Platform,
+			SessionId: conn.SessionId,
+			Token:     conn.Token,
+		},
+	)
+	return agg.Create(ctx)
 }
 
 func DeleteConn(ctx context.Context, conn *model.Conn) error {
 	connAgg := websocket.FactoryConnAgg(ctx, conn)
-	return connAgg.Delete(ctx)
+	if err := connAgg.Delete(ctx); err != nil {
+		return err
+	}
+	// 发布一个离线事件.
+	agg := websocket.FactoryMessageAgg(
+		ctx, &model.Message{
+			Event:     constant.Offline,
+			Platform:  conn.Platform,
+			SessionId: conn.SessionId,
+			Token:     conn.Token,
+		},
+	)
+	return agg.Create(ctx)
 }
